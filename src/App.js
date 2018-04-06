2ba82650fd3851d304d00ec1a7493d01db7973f5
Roni <roni.p.ahti@gmail.com>
2018-04-06 10:30:18 +0000

    react databinding alku

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {value : ''}
+  } 
+  handleChange = (e) => {
+    this.setState({value: e.target.value});
+  }
   render() {
     return (
+      
       <div className="App">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.js"></script>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +23,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <div>{this.state.value}</div>
+        </div>
       </div>
+      
     );
   }
 }
